8428bea916908cd2f098e52345ef99e9277e8085
bryan <bryan.dov.bergman@gmail.com>
2022-10-15 14:01:46 +0300

    Initial commit

---
+++ app/src/main/java/com/company/mathgame/Game.java
@@ -0,0 +1,19 @@
+package com.company.mathgame;
+
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Game extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game);
+
+
+    }
+}
---
+++ app/src/main/java/com/company/mathgame/MainActivity.java
@@ -0,0 +1,30 @@
+package com.company.mathgame;
+
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    Button mAddBtn, mSubBtn, mMultiBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mAddBtn = findViewById(R.id.buttonAdd);
+        mSubBtn = findViewById(R.id.buttonSub);
+        mMultiBtn = findViewById(R.id.buttonMult);
+
+        mAddBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Game.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
